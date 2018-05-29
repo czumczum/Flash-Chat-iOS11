223cbbe6557893819dc91796bee5158b68e836e6
Ula <slodkosci@gmail.com>
2018-05-29 15:06:59 -0400

    Custom message + Message Class

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,6 +6,7 @@
 
 
 import UIKit
+import Firebase
 
 
 class LogInViewController: UIViewController {
@@ -26,8 +27,16 @@ class LogInViewController: UIViewController {
    
     @IBAction func logInPressed(_ sender: AnyObject) {
 
-        
-        //TODO: Log in the user
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print("There was some errors!")
+                print(error!)
+            } else {
+                print("Login succesful!")
+                
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
         
     }
