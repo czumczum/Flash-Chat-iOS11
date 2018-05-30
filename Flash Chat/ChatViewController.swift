fd89f8ca46185eff3c9ab2d53dbcd1aed7081f3e
Ula <slodkosci@gmail.com>
2018-05-30 12:11:53 -0400

    Messages are now connected to the firebase; different look for current user messages

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,112 +10,148 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
-    // Declare instance variables here
+    //MARK: - Instance variables
+    var messageArray : [Message] = [Message] ()
+    
 
-    
+    //MARK: - Outlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
     @IBOutlet var sendButton: UIButton!
     @IBOutlet var messageTextfield: UITextField!
     @IBOutlet var messageTableView: UITableView!
     
     
-    
+    //MARK: - Loading the view methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         messageTableView.delegate = self
         messageTableView.dataSource = self
         
-        
-        //TODO: Set yourself as the delegate of the text field here:
-
+        messageTextfield.delegate = self
         
         
-        //TODO: Set the tapGesture here:
+        //tapGesture
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
         
         
-
+        //adding custom cells' layout
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         configureTableView()
+        retrieveMessages()
 
-        
     }
-
+    
     ///////////////////////////////////////////
     
+    
     //MARK: - TableView DataSource Methods
-    
-    
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Message", "Third message"]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         
-        cell.messageBody.text = messageArray[indexPath.row]
+        if cell.senderUsername.text == Auth.auth().currentUser?.email {
+            cell.messageBackground.backgroundColor = UIColor(red: 0.4, green: 0.2, blue: 0.2, alpha: 0.4)
+        }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
+        
     }
     
-    
-    //TODO: Declare tableViewTapped here:
-    
+    ///////////////////////////////////////////
     
     
-    //TODO: Declare configureTableView here:
+    //tableViewTapped method for custom Gesture
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
+    
+    //configureTableView
     func configureTableView() {
         messageTableView.rowHeight = UITableViewAutomaticDimension
         messageTableView.estimatedRowHeight = 120.0
     }
     
+    //MARK: - TextField Delegate Methods
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     
-    
-    ///////////////////////////////////////////
-    
-    //MARK:- TextField Delegate Methods
-    
-    
-
-    
-    //TODO: Declare textFieldDidBeginEditing here:
-    
-    
-    
-    
-    //TODO: Declare textFieldDidEndEditing here:
-    
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.3) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
     
     ///////////////////////////////////////////
     
     
     //MARK: - Send & Recieve from Firebase
     
-    
-    
-    
-    
     @IBAction func sendPressed(_ sender: AnyObject) {
+        messageTextfield.endEditing(true)
         
+        //MARK: Send the message to Firebase and save it in the database
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
         
-        //TODO: Send the message to Firebase and save it in our database
+        let messagesDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email,
+                                  "MessageBody": messageTextfield.text!]
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, refrence) in
+            if error != nil {
+                print("error")
+            } else {
+                print("Message sent succesfuly")
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextfield.text = ""
+            }
+        }
         
         
     }
     
-    //TODO: Create the retrieveMessages method here:
+    //MARK: - retrieveMessages method
+    func retrieveMessages() {
+        let messagesDB = Database.database().reference().child("Messages")
+        messagesDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String,String>
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            self.configureTableView()
+            self.messageTableView.reloadData()
+        }
+    }
     
     
 
-    
+     ///////////////////////////////////////////
     
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
